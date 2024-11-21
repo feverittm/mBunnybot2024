a7496b129af5f260d2132d194745ce264f49ec23
Floyd Moore <floyd.moore@gmail.com>
2024-11-21 13:53:50 -0800

    updatea

---
+++ README.md
@@ -0,0 +1,3 @@
+# 2024 Bunnybot
+
+https://github.com/Team997Coders/2024BunnyBot/wiki
---
+++ src/main/java/frc/robot/commands/DefaultDrive.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class DefaultDrive extends Command {
+  private final DrivetrainSubsystem m_drive;
+  private final double m_forward;
+  private final double m_rotation;
+
+  public DefaultDrive(DrivetrainSubsystem subsystem, double forward, double rotation) {
+    m_drive = subsystem;
+    m_forward = forward;
+    m_rotation = rotation;
+    addRequirements(m_drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.drive(m_forward, m_rotation);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
